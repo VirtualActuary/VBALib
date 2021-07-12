20e8017c825a373481fd0d9212bb7d318a7638a1
heetbeet <example@example.com>
2021-07-12 16:56:49 +0200

    Added exstra stuff

--- generate-vba-library.py
+++ generate-vba-library.py
@@ -1,7 +1,32 @@
 from zebra_vba_packager import Config, Source
 from locate import this_dir
+from textwrap import dedent 
+import os
+import subprocess
+import shutil
+from contextlib import suppress 
+from pathlib import Path
+
 
 output = this_dir().joinpath("output")
+
+def create_output_workbook():
+    interm_output_xl = this_dir().joinpath("output--compiled.xlsb")
+    output_xl = this_dir().joinpath("output.xlsb")
+
+    vbs_script = this_dir().joinpath("compile-to-workbook", "compile.vbs")
+    empty_xl = this_dir().joinpath("compile-to-workbook", "output.xlsx")
+
+    with suppress(FileNotFoundError):
+        os.unlink(interm_output_xl)
+
+    shutil.copy2(empty_xl, output.joinpath(empty_xl.name))
+    subprocess.call(["cscript", str(vbs_script), output])
+    shutil.copy2(interm_output_xl, output_xl)
+
+    with suppress(FileNotFoundError):
+        os.unlink(interm_output_xl)
+
 
 def mid_process(source):
     for pth in source.temp_transformed.rglob("*.bas"):
@@ -22,6 +47,115 @@ def mid_process(source):
                 f.write((b"\r\n".join(txt_lines)))
 
 
+# Seperate step for this library
+common_lib = Config(
+    Source(
+        git_source="https://github.com/nylen/vba-common-library.git",
+        git_rev="1e21b0d",
+        glob_include=['*.cls', '*.bas'],
+        glob_exclude=['*VBALib_VERSION*'],
+        auto_bas_namespace=False,
+        auto_cls_rename=False
+    )
+)
+common_lib.run()
+
+all_bas_lines = []
+all_pointer_declare = []
+all_precode_declare = []
+for fpath in common_lib.output_dir.glob("*.bas"):
+    if "z__" in str(fpath) and str(fpath).lower().endswith(".bas"): 
+        continue
+
+    with fpath.open("rb") as f:
+        txt_lines = f.read().split(b"\r\n")
+
+    # strip trailing ending lines
+    i = -1
+    while (i := i+1) < len(txt_lines):
+        if txt_lines[i].strip().lower().split()[1:2] == [b"declare"]:
+            j = i
+            for j in range(i, len(txt_lines)):
+                if not txt_lines[j].strip().endswith(b"_"):
+                    break
+            line = b" ".join([txt_lines[ii].strip().rstrip(b"_") for ii in range(i,j+1)])
+
+            txt_lines[i] = line
+            for ii in range(i,j):
+                txt_lines.pop(i+1)
+
+    i = -1
+    while (i := i+1) < len(txt_lines):
+        if txt_lines[i].strip().lower().split()[1:2] == [b"declare"]:
+            txt64 = txt_lines[i].replace(b" Declare ", b" Declare PtrSafe ").replace(b"Long", b"LongLong")
+            all_pointer_declare.append(dedent(f"""
+            #If VBA7 Then
+                {txt64.decode('utf-8')}
+            #Else
+                {txt_lines[i].decode('utf-8')}
+            #End If
+            """).strip().encode("utf-8"))
+
+
+            txt_lines.pop(i)
+            i -= 1
+
+    i = -1
+    while (i := i+1) < len(txt_lines):
+        funcdecl = txt_lines[i].lower().strip().split()[:2]
+        if (funcdecl[:1] == [b"function"] or funcdecl[:1] == [b"sub"] or
+            funcdecl == [b"private", b"function"] or funcdecl == [b"private", b"sub"] or
+            funcdecl == [b"public", b"function"] or funcdecl == [b"public", b"sub"]):
+            break
+
+    all_precode_declare.extend([j for j in txt_lines[1:i] if not j.lower().strip().startswith(b"option ")])
+    all_bas_lines.extend(txt_lines[i:])
+    os.remove(fpath)
+
+for fpath in common_lib.output_dir.glob("*.cls"):
+    with fpath.open("rb") as f:
+        txt = f.read()
+
+    with fpath.open("wb") as f:
+        f.write(txt.replace(b"Attribute VB_Exposed = False",
+                            b"Attribute VB_Exposed = True"))
+
+
+with common_lib.output_dir.joinpath("concatenated.bas").open("wb") as f:
+    f.write(b"\r\n".join(
+        [b'Attribute VB_Name = "VLib"']+
+        all_pointer_declare+
+        [b"Option Explicit"]+
+        all_precode_declare+
+        all_bas_lines
+        ))
+
+# Find all function/sub names and attach "VLib." in front ot them
+vlib_funcnames = set()
+for i in all_bas_lines:
+    fline = i.strip().split()
+    with suppress(IndexError):
+        if fline[0].lower() in (b"function", b"sub") or fline[1].lower() in (b"function", b"sub"):
+            vlib_funcnames.add(fline[1].decode("utf-8").split("(")[0])
+            vlib_funcnames.add(fline[2].decode("utf-8").split("(")[0])
+
+vlib_namespace_fix = [(i, "VLib."+i) for i in vlib_funcnames]
+vlib_renames = [       
+    (lambda x: x.lower().startswith("vbalib_excel"),
+     lambda x: "zVLib"+x[len("vbalib_excel"):]),
+
+    (lambda x: x.lower().startswith("vbalib_"),
+     lambda x: "zVLib"+x[len("vbalib_"):]),
+]
+
+common_lib_output_dir2 = Path(str(common_lib.output_dir)+"2")
+shutil.rmtree(common_lib_output_dir2, ignore_errors=True)
+shutil.copytree(common_lib.output_dir, common_lib_output_dir2)
+
+
+#-----------------------------------------------------------------------
+# Aggregate all the rest of the sources
+#-----------------------------------------------------------------------
 Config(
     Source(
         git_source="https://github.com/ws-garcia/VBA-CSV-interface.git",
@@ -74,45 +208,40 @@ Config(
         },
 
     ),
-    #Source(
-    #    git_source="https://github.com/nylen/vba-common-library.git",
-    #    git_rev="1e21b0d",
-    #    glob_include=['**/VBALib_ExcelTable.cls', '**/VBALib_ExcelUtils.bas'],
-    #    auto_bas_namespace=False,
-    #    rename_overwrites={
-    #        "VBALib_ExcelTable": "zListObject",
-    #        "VBALib_ExcelUtils": "z__ListObject",
-    #        "GetExcelTable": "zGetListObject"
-    #    }
-    #),
     Source(
         git_source="https://github.com/todar/VBA-Strings",
         git_rev="6d25dad",
         glob_include=["*.bas"],
         rename_overwrites={
-            "StringFunctions":"StrUtils"
+            "StringFunctions": "StrUtils"
+        }
+    ),
+    Source(
+        path_source=common_lib.output_dir,
+        glob_include=["*.cls"],
+        glob_exclude=["z__*"],
+        rename_overwrites=vlib_namespace_fix+vlib_renames
+    ),
+    Source(
+        path_source=common_lib_output_dir2,
+        glob_include=["*.bas"],
+        glob_exclude=["z__*"],
+        rename_overwrites=vlib_renames
+    ),
+    Source(
+        path_source=str(this_dir().joinpath("add_early_bindings")),
+        auto_bas_namespace=False,
+        rename_overwrites={
+            "EarlyBindings": "z__EarlyBindings"
         }
     )
 ).run(
     output
 )
 
-# Turn off early bindings for "compress" module
-cmpr = output.joinpath("z__Compress__.cls")
-with cmpr.open("rb") as f:
-    txt = f.read().replace(b"#Const EarlyBinding = True",
-                           b"#Const EarlyBinding = False")
-with cmpr.open("wb") as f:
-    f.write(txt)
 
+create_output_workbook()
 
-## Strip everything but the ExcelTable Factory
-#lo = output.joinpath("z__ListObject.bas")
-#with lo.open("rb") as f:
-#    txt_lines = f.read().split(b"\r\n")
-#
-#with lo.open("wb") as f:
-#    f.write(b"\r\n".join(txt_lines[:5]+txt_lines[400:435]))
 
 
 
